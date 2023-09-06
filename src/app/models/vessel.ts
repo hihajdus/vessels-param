903d0adef876875fe3d2fb647fa635c8a84dcd30
hihajdus <sebastian.hajdus@gmail.com>
2023-09-06 21:53:01 +0200

    Model interface vessel

---
+++ src/app/models/vessel.ts
@@ -0,0 +1,11 @@
+export interface Vessel {
+    id: number;
+    name: string;
+    mmsi: number;
+    imo: number;
+    companyId: number;
+    companyName: string;
+    startDate: Date;
+    active: boolean;
+    vesselType: string;
+}
