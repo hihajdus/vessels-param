04477df2b35ddb6bce71a3551087ce252a3ceeae
hihajdus <sebastian.hajdus@gmail.com>
2023-09-12 13:14:14 +0200

    Auto-generated commit [ci skip]

--- src/app/models/emission.ts
+++
@@ -1,9 +0,0 @@
-export interface Emission {
-    report_from_utc: string;
-    report_to_utc: string;
-    co2_emissions: number;
-    sox_emissions: number;
-    nox_emissions: number;
-    pm_emissions: number;
-    ch4_emissions: number;
-}
